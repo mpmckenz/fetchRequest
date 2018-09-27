3ce2ef49d01d071167c149f3329d084ad8a0b840
Michael McKenzie <mikemckenzie987@gmail.com>
2018-09-27 00:53:23 -0400

    first one done

--- script.js
+++ script.js
@@ -1,64 +1,38 @@
-/* Clues for: Around the World-- http://jservice.io/popular/1079
-http://jservice.io/api/category?id=1079
-Clues for: Around the World-- http://jservice.io/popular/777
-http://jservice.io/api/category?id=777
+const fetchResults1 = document.getElementById("fetchResults")
 
-4. For one of your two categories, write a GET request using fetch() and .then()
- to display the category title and one random clue from this category on the DOM. 
- (5 points)
-5. For the other of your two categories, write a GET request using fetch() and 
-async/await to display the category title and one random clue from this category
- on the DOM. (5 points) */
+function jeopardyContainer1(clues) {
+    let categoryHeader = document.createElement("h3")
+    let container = document.createElement("div")
+    let categoryNode = document.createTextNode(clues.Category)
+    let clueNode = document.createTextNode(clues.Clue)
+    let answerNode = document.createTextNode(clues.Answer)
+    categoryHeader.appendChild(categoryNode)
+    container.appendChild(categoryHeader)
+    container.appendChild(clueNode)
+    container.appendChild(answerNode)
+    fetchResults1.appendChild(container)
+}
 
-// Class Clue {
-
-// constructor () {
-    // this.randomClue()
-//         .then(clue => {
-//             this.category = clue.title
-//             this.question = clue.question
-//             this.answer = clue.answer
-//         })
-// }
-// }
-
-//     randomClue () {
-
-    // let jeopardyMsg = 
-    // `Category: ${categoryTitle}\nQuestion: ${clue.question}`
-    // let container = document.createElement("div")
-    // let jeopardyNode = document.createTextNode(jeopardyMsg)
-    // let clueNode = document.createTextNode(clue)
-    // container.appendChild(jeopardyNode)
-    // container.appendChild(clueNode)
-    // document.body.appendChild(container)
-    // console.log(clueNode)
+fetch("http://jservice.io/api/category?id=1079")
+    .then(promisedResponse => promisedResponse.json())
+    .then(payload => {
+        let categoryTitle = payload.title
+        let randomClue = payload.clues[Math.floor(Math.random() * 
+            payload.clues.length)]
+        let randomAnswer = payload.clues
 
 
+        const aroundTheWorld1 = {
+            Category: "Category: " + categoryTitle,
+            Clue: "Clue: " + randomClue.question,
+            Answer: " Answer: " + randomAnswer.answer
+        }
 
-// fetch ("http://jservice.io/api/category?id=1079")
-//     .then(promisedResponse => promisedResponse.json())
-//     .then(payload => {
-//         let categoryTitle = payload.title
-//         let clue = payload.clues[Math.floor(Math.random() * payload.clues.length)]
-
-//             let jeopardyMsg = 
-//             `Category: ${categoryTitle}\nQuestion: ${clue.question}`
-//             let container = document.createElement("div")
-//             let jeopardyNode = document.createTextNode(jeopardyMsg)
-//             let clueNode = document.createTextNode(clue)
-//             container.appendChild(jeopardyNode)
-//             container.appendChild(clueNode)
-//             document.body.appendChild(container)
-//             console.log(container)
-//         })
+        jeopardyContainer1(aroundTheWorld1)
+        console.log(randomAnswer)
+    })
 
 
-// fetch ("http://jservice.io/api/category?id=1079")
-//     .then(promisedResponse => promisedResponse.json())
-//     .then(hydratedClue => console.log(hydratedClue))
-
-//     function displayClue (hydratedClue) {
-//         clue = hydratedClue
-
-//     }
+fetch("http://jservice.io/api/category?id=1079")
+    .then(promisedResponse => promisedResponse.json())
+    .then(hydratedClue => console.log(hydratedClue))
